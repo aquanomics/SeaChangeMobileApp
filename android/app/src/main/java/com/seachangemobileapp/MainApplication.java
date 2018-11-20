dca2ff5bb190082e8e6bfc9ae07c885c91bd4c24
Alexander Julianto <alexander.julianto94@gmail.com>
2018-11-19 22:08:39 -0800

    Fix android duplicate dependencies

--- android/app/src/main/java/com/seachangemobileapp/MainApplication.java
+++ android/app/src/main/java/com/seachangemobileapp/MainApplication.java
@@ -27,8 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new MapsPackage(),
-            new RNGestureHandlerPackage(),
-          new MapsPackage()
+            new RNGestureHandlerPackage()
       );
     }
 

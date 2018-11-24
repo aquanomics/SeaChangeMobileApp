c3abd2e364585e01989afb565b3a6a0488bfa818
benkenj <benkenjim@gmail.com>
2018-11-24 13:36:27 -0800

    Style map screen and add two new modules
    react-native-action-button added.
    react-native-vector-icons added.
    currently works on ios, some setup may be needed for android.

--- components/UserMap.js
+++ components/UserMap.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {View, Button, StyleSheet,Text } from "react-native";
+import {View, Button, StyleSheet,Text ,TouchableOpacity} from "react-native";
 import MapView from "react-native-maps";
 import { createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
 import ActionButton from 'react-native-action-button';
@@ -48,81 +48,89 @@ export default class UserMap extends Component{
     return (
     
     <View style={{ flex: 1 }}>
-    
-      <MapView style={{ flex: 1 }} />
-      <View
-          style={{
-              position: 'absolute',//use absolute position to show button on top of the map
-              bottom: '20%', //for center align
-              right: '20%',
-              backgroundColor: '#f3f3f3'
-          }}
-      >
-      <View style={{width:60, backgroundColor: '#f3f3f3'}}>
-        {/* Rest of the app comes ABOVE the action button component !*/}
-        <ActionButton buttonColor="rgba(231,76,60,1)">
-          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
-            <Icon name="md-create" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
-            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
-          </ActionButton.Item>
-          
-        </ActionButton>
+      <View style={{height:"75%"}}>
+        <MapView style={{ flex: 1 }} />
+        <View
+            style={{
+                position: 'absolute',//use absolute position to show button on top of the map
+                bottom: '20%', //for center align
+                right: '20%',
+                backgroundColor: '#f3f3f3'
+            }}
+        >
+          <View style={{width:60, backgroundColor: '#f3f3f3'}}>
+            {/* Rest of the app comes ABOVE the action button component !*/}
+            <ActionButton buttonColor="rgba(255,255,255,1)" buttonTextStyle={{color:'#3498db'}}>
+              <ActionButton.Item buttonColor='#3498db'  onPress={() => console.log("notes tapped!")}>
+                <Icon name="md-create" style={styles.actionButtonIcon} />
+              </ActionButton.Item>
+              <ActionButton.Item buttonColor='#3498db' onPress={() => {}}>
+                <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+              </ActionButton.Item>
+              
+            </ActionButton>
+          </View>
+            
+        </View>
       </View>
 
-        
-          {/* Rest of the app comes ABOVE the action button component !*/}
+      <View style={styles.menu}>
 
-          
+        <View style={{flexDirection: 'row',flexWrap: 'wrap',justifyContent: 'center', alignItems: 'center',}}>
+         <TouchableOpacity style={styles.menuButton} onPress={() => this.props.navigation.navigate('Events')}>
+            <Icon name={"md-create"}  size={30} color="#01a699" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuButton} onPress={() => this.props.navigation.navigate('Events')}>
+            <Icon name={"md-create"}  size={30} color="#01a699" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuButton} onPress={() => this.props.navigation.navigate('Events')}>
+            <Icon name={"md-create"}  size={30} color="#01a699" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuButton} onPress={() => this.props.navigation.navigate('Events')}>
+            <Icon name={"md-create"}  size={30} color="#01a699" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuButton} onPress={() => this.props.navigation.navigate('Events')}>
+            <Icon name={"md-create"}  size={30} color="#01a699" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.menuButton} onPress={() => this.props.navigation.navigate('Events')}>
+            <Icon name={"md-create"}  size={30} color="#01a699" />
+          </TouchableOpacity>   
+        </View>
 
       </View>
+
     </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex:1
-    },
-    mapContainer: {
-      flex:1,
-      
-      justifyContent: 'center',
-    alignItems: 'center'
-    },
-    but: {
-      color:"#841584",
-      backgroundColor: '#2196F3'
-    },
+
     actionButtonIcon: {
       fontSize: 20,
       height: 22,
       color: 'white',
     },
 
-
-    buttonView:{
-      position: 'absolute',
-      top: '80%' 
-    },
-
-
-    map: {
-      flex:1,
-      zIndex: -1
-      
-    },
     menu: {
-      height: "10%",
+      height: "25%",
       flex: 1, 
-      flexDirection: 'row'
+      flexDirection: 'row',  
+      paddingLeft: 20
     },
-    menuButton: {
-      height: "100%",
-      width: "30%"
 
+    menuButton: { 
+      borderWidth:1,
+      borderColor:'rgba(0,0,0,0.2)',
+      alignItems:'center',
+      justifyContent:'center',
+      width:55,
+      height:55,
+      backgroundColor:'#fff',
+      borderRadius:55,
+      marginLeft: 20,
+      marginRight: 20,
+      marginTop: 15
     }
 });
 

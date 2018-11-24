87a11b1d0f6c7d55726a89c9a74becbf869342d6
benkenj <benkenjim@gmail.com>
2018-11-24 00:42:34 -0800

    minor styling on action button

--- components/UserMap.js
+++ components/UserMap.js
@@ -48,29 +48,34 @@ export default class UserMap extends Component{
     return (
     
     <View style={{ flex: 1 }}>
+    
       <MapView style={{ flex: 1 }} />
       <View
           style={{
               position: 'absolute',//use absolute position to show button on top of the map
-              top: '10%', //for center align
+              bottom: '20%', //for center align
               right: '20%',
-              alignSelf: 'flex-end' //for align to right
+              backgroundColor: '#f3f3f3'
           }}
       >
-        <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
+      <View style={{width:60, backgroundColor: '#f3f3f3'}}>
+        {/* Rest of the app comes ABOVE the action button component !*/}
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+            <Icon name="md-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          
+        </ActionButton>
+      </View>
+
+        
           {/* Rest of the app comes ABOVE the action button component !*/}
-          <ActionButton buttonColor="rgba(231,76,60,1)">
-            <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
-              <Icon name="md-create" style={styles.actionButtonIcon} />
-            </ActionButton.Item>
-            <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
-              <Icon name="md-notifications-off" style={styles.actionButtonIcon} /><Icon name="md-done-all" style={styles.actionButtonIcon} />
-           </ActionButton.Item>
-            <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
-              <Icon name="md-done-all" style={styles.actionButtonIcon} />
-            </ActionButton.Item>
-          </ActionButton>
-        </View>
+
+          
+
       </View>
     </View>
     );

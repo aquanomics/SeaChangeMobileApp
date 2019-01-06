6626eb9a48d3c4f4bba4534c1dc4a5da4b4cabc1
Benjamin Hong <benswh@gmail.com>
2019-01-05 22:18:20 -0800

    Added filter. Changed db schema & changed server code

--- components/ArticlePageComponent/news.js
+++ components/ArticlePageComponent/news.js
@@ -1,8 +1,8 @@
 const url =
-  "http://seachange.ca-central-1.elasticbeanstalk.com/api/newsarticle?fbclid=IwAR1hn8OhP30Kp3_WTAvJwIG-JoelrY0h1RBKbw2f4z1PdM1w39t2IGTXD6g";
+  "http://seachange.ca-central-1.elasticbeanstalk.com/api/newsarticle?category=";
 
-export async function getNews() {
-  let result = await fetch(url).then(response => response.json());
+export async function getNews(category) {
+  let result = await fetch(url+category).then(response => response.json());
   console.log(result);
   return result.NewsArticle;
 }
--- components/SettingsPage.js
+++ components/SettingsPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {View,Text, StyleSheet, Button } from "react-native";
+import {View,Text, StyleSheet, Button} from "react-native";
 import firebase from 'react-native-firebase';
 
 export default class SettingsPage extends Component{

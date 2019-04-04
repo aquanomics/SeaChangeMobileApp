29d6ebb7fa9f0ba4591f9b442edc7e39a92d05e8
Benjamin Hong <benswh@gmail.com>
2019-04-04 13:24:04 -0700

    Fish details page loading bug fix

--- components/FishDetailPage.js
+++ components/FishDetailPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet, Text, View, FlatList
+  StyleSheet, Text, View, FlatList, NetInfo
 } from 'react-native';
 import { getSpeciesDetails } from './FishPageComponent/Fish';
 import Details from './FishPageComponent/Details';
@@ -12,8 +12,9 @@ export default class FishDetailPage extends Component {
       data: [],
       refreshing: true,
       category: 'animal',
-      isLoading: true,
       fetching_Status: false,
+      connection_Status : "",
+      noResultReturned: false,
     };
 
     this.fetchDetails = this.fetchDetails.bind(this);
@@ -22,14 +23,38 @@ export default class FishDetailPage extends Component {
   }
 
   componentDidMount() {
+    // Used to detect network status change
+    NetInfo.isConnected.addEventListener(
+      'connectionChange',
+      this._handleConnectivityChange
+    );
+
+    NetInfo.isConnected.fetch().done((isConnected) => {
+      if (isConnected == true) this.setState({ connection_Status: 'Online' });
+      else this.setState({ connection_Status: 'Offline' });
+    });
+
     this.fetchDetails(this.state.category);
     this.props.navigation.setParams({ fetchDetails: this.Details });
   }
 
+  componentWillUnmount() {
+    NetInfo.isConnected.removeEventListener(
+      'connectionChange',
+      this._handleConnectivityChange
+    );
+  }
+
+  _handleConnectivityChange = (isConnected) => {
+    if (isConnected == true) this.setState({ connection_Status: 'Online' });
+    else this.setState({ connection_Status: 'Offline' });
+  };
+
   fetchDetails = () => {
     getSpeciesDetails(this.SpeciesCode)
       .then((response) => {
-        this.setState({ data: response, refreshing: false });
+        response.length == 0 ? this.setState({data: [], refreshing: false, noResultReturned: true}) :
+          this.setState({ data: response, refreshing: false, noResultReturned: false });
       })
       .catch(() => { this.setState({ data: [], refreshing: false }); console.log('ERROR'); });
   }
@@ -40,14 +65,18 @@ export default class FishDetailPage extends Component {
 
   render() {
     return (
-      <DisplaySpecies
-        data={this.state.data}
-        refreshing={this.state.refreshing}
-        handleRefresh={this.handleRefresh.bind(this)}
-          // handleFetchMore={this.handleFetchMore.bind(this)}
-        key={this._keyExtractor}
-        navigation={this.props.navigation}
-      />
+      <View style={styles.myContainer}>
+        <DisplaySpecies
+          data={this.state.data}
+          refreshing={this.state.refreshing}
+          handleRefresh={this.handleRefresh.bind(this)}
+            // handleFetchMore={this.handleFetchMore.bind(this)}
+          key={this._keyExtractor}
+          navigation={this.props.navigation}
+          connection_Status={this.state.connection_Status}
+          noResultReturned={this.state.noResultReturned}
+        />
+      </View>
     );
   }
 }
@@ -60,16 +89,47 @@ function DisplaySpecies(props) {
       renderItem={({ item }) => <Details navigation={props.navigation} details={item} index={item.index} />}
       refreshing={props.refreshing}
       onRefresh={props.handleRefresh}
-      ListEmptyComponent={<DisplayNoInternet styles={styles} />}
+      ListEmptyComponent={
+        <DisplayNoInternet 
+          styles={styles} 
+          connection_Status={props.connection_Status}
+          refreshing={props.refreshing}
+          noResultReturned={props.noResultReturned}
+        />
+      }
     />
   );
 }
 
 function DisplayNoInternet(props) {
+  if (props.connection_Status == 'Offline') {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>No Internet</Text>
+      </View>
+    );
+  }
+  // Note: need to check for undefined because render functions are ran before constructor() is ran which renders (no pun intended)
+  // all state variables undefined
+  if (props.refreshing || props.refreshing === undefined) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Loading</Text>
+      </View>
+    );
+  } if (props.noResultReturned == true) {
+    // empty case
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>No results</Text>
+        <Text style={styles.instructions}>If connection was lost previously, try again</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.welcome}>Cannot Load Information</Text>
-      <Text style={styles.instructions}>Might want to check your internet</Text>
+      <Text style={styles.welcome}>Cannot Load Fish</Text>
+      <Text style={styles.instructions}>If connection was lost previously, try again</Text>
     </View>
   );
 }
@@ -79,5 +139,26 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  myContainer: {
+    flex: 1,
+    backgroundColor: '#8cdff2'
+    // paddingTop: Constants.statusBarHeight,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
